0ac4e733189321bc439659d5cd16cd68a9529b2d
BrunoGG69 <prathameshprabhakar27@gmail.com>
2024-12-09 01:17:19 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,jsx}'], // Ensure Tailwind scans all necessary files
+export default {
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'saved-by-zero': ['"Saved by Zero"', 'sans-serif'], // Add the font here
+        'saved-by-zero': ['"Saved by Zero"', 'sans-serif'],
       },
     },
   },
