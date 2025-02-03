248842e7484dca1107ef5e7c931144968de22427
BrunoGG69 <prathameshprabhakar27@gmail.com>
2025-02-03 12:41:06 +0530

    Started design on new landing page

--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -13,12 +13,6 @@ const Hero = () => {
                 <h1 className="text-[300px] font-bold font-pacifico">Wavify</h1>
                 <p className="mt-4 text-lg">Your music, your vibe.</p>
                 <div className="mt-6 space-x-4">
-                    <button className="px-6 py-2 bg-[#FF00FF] hover:bg-[#FF00FF]/90 text-white rounded-lg">
-                        Get Started
-                    </button>
-                    <button className="px-6 py-2 text-white border border-white rounded-lg hover:bg-white hover:text-black">
-                        Sign In
-                    </button>
                 </div>
             </div>
         </div>
